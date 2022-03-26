3cb5ed95adc37a0a709991d826e55560abce8fdf
Rahat47 <djrayhan8@gmail.com>
2022-03-26 21:59:17 +0600

    token checker page contents and layout done

--- src/containers/TokenCheckerPage/TokenCheckerHeading.jsx
+++ src/containers/TokenCheckerPage/TokenCheckerHeading.jsx
@@ -39,7 +39,7 @@ const routes = [
 
 const TokenCheckerHeading = () => {
     return (
-        <div className='mx-11 flex flex-col items-center justify-center'>
+        <div className='flex flex-col items-center justify-center sm:mx-4 md:mx-11'>
             <img
                 src={tokenCheckerImg}
                 alt='token checker'
@@ -57,7 +57,7 @@ const TokenCheckerHeading = () => {
                 </div>
             </div>
 
-            <div className='flex items-center space-x-1 place-self-start'>
+            <div className='checkerlinks mb-2 flex w-full items-center space-x-1 place-self-start overflow-x-auto'>
                 {routes.map(item => (
                     <NavLink
                         to={item.path}
@@ -68,7 +68,7 @@ const TokenCheckerHeading = () => {
                                     'border-b-[5px] border-b-[#211f1f] !text-white':
                                         isActive,
                                 },
-                                'flex items-center space-x-1 border-[5px] border-b-0 border-l-[#211f1f]  border-t-[#494949] border-r-[#494949] bg-[#262626] py-[10px] px-5 font-semibold text-[#e7cd86]'
+                                'flex items-center space-x-1 whitespace-nowrap border-[5px] border-b-0 border-l-[#211f1f]  border-t-[#494949] border-r-[#494949] bg-[#262626] px-2 py-2 font-semibold text-[#e7cd86] md:px-5 md:py-[10px]'
                             )
                         }
                         end
@@ -77,6 +77,15 @@ const TokenCheckerHeading = () => {
                         <span>{item.label}</span>
                     </NavLink>
                 ))}
+
+                <div className='flex items-center justify-center space-x-4 whitespace-nowrap pl-5'>
+                    <button className='flex h-9 items-center justify-center rounded-2xl bg-red-600 p-3 text-white md:hidden'>
+                        <span>Make a Challange</span>
+                    </button>
+                    <button className='flex h-9 items-center justify-center rounded-2xl bg-primary-brand p-3 text-white md:hidden'>
+                        <span>Watchlist Tokens</span>
+                    </button>
+                </div>
             </div>
         </div>
     );
--- src/pages/TokenCheckerPage.jsx
+++ src/pages/TokenCheckerPage.jsx
@@ -1,17 +1,34 @@
 import { Outlet, useParams } from 'react-router-dom';
-import { CoinsTable, TokenCheckerHeading } from '../containers';
+import {
+    CoinsTable,
+    TokenCheckerHeading,
+    TokenCheckerInfo,
+} from '../containers';
 
 const TokenCheckerPage = () => {
     const { filter } = useParams();
 
     return (
-        <main className='max-w-7xl bg-primary-sidebar px-6 pt-20'>
-            <TokenCheckerHeading />
+        <>
+            <main className='max-w-7xl bg-primary-sidebar px-6 pt-20'>
+                <TokenCheckerHeading />
 
-            {!filter && <CoinsTable />}
+                {!filter && <CoinsTable />}
 
-            <Outlet />
-        </main>
+                <Outlet />
+
+                <TokenCheckerInfo />
+            </main>
+            <div className='mt-24 bg-gradient-to-b from-white to-[#dbdbdb] px-6 pb-5'>
+                <div className='container max-w-5xl'>
+                    <span className='float-right font-semibold text-[#777777]'>
+                        Want to advertise with us?{' '}
+                        <span className='text-primary-brand'>click here</span>{' '}
+                        the more info
+                    </span>
+                </div>
+            </div>
+        </>
     );
 };
 
