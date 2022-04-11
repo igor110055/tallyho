0d2b281d58c4aa2710fdaf7efab22085dcd64a48
Rahat47 <djrayhan8@gmail.com>
2022-04-11 19:13:15 +0600

    slideshow issue sorted

--- src/components/HomePage/SlideshowHome.jsx
+++ src/components/HomePage/SlideshowHome.jsx
@@ -2,29 +2,46 @@ import { Carousel } from 'react-responsive-carousel';
 import gif1 from '../../assets/images/gifs/Tally-ho_Home 2.gif';
 import gif2 from '../../assets/images/gifs/tally-ho_MOD.gif';
 import gif3 from '../../assets/images/gifs/Tally-ho_Home Page 3.gif';
+import { useState } from 'react';
 
 const SlideshowHome = () => {
+    const [interval, setInterval] = useState(9000);
+
+    const handleOnChange = (index, item) => {
+        setInterval(item.props['data-interval']);
+    };
+
     return (
         <Carousel
             showArrows={false}
+            onChange={handleOnChange}
             showThumbs={false}
             showStatus={false}
             showIndicators={false}
             autoPlay={true}
             infiniteLoop={true}
-            interval={13000}
+            interval={interval}
             centerMode={true}
             centerSlidePercentage={100}
             className='w-fit'
         >
-            <div className='mx-auto w-full max-w-md lg:ml-auto lg:mr-0'>
-                <img src={gif1} alt='gif1' className='w-full' />
+            <div
+                className='mx-auto w-full max-w-md lg:ml-auto lg:mr-0'
+                data-interval={9000}
+            >
+                <img src={gif1} alt='gif1' className='w-full' loading='lazy' />
             </div>
-            <div className='mx-auto w-full max-w-md lg:ml-auto lg:mr-0'>
-                <img src={gif2} alt='gif2' className='w-full' />
+            <div
+                className='mx-auto w-full max-w-md lg:ml-auto lg:mr-0'
+                data-interval={7000}
+            >
+                <img src={gif2} alt='gif2' className='w-full' loading='lazy' />
             </div>
-            <div className='mx-auto w-full max-w-md lg:ml-auto lg:mr-0'>
-                <img src={gif3} alt='gif3' className='w-full' />
+            <div
+                className='mx-auto w-full max-w-md lg:ml-auto lg:mr-0'
+                data-interval={10000}
+            >
+                <img src={gif3} alt='gif3' className='w-full' loading='lazy' />
             </div>
         </Carousel>
     );
